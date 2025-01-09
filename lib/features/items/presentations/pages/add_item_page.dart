eb59cee2d167e3976c754354a9bb0f388b017760
Ghofran <gh.malket@gmail.com>
2025-01-09 23:27:41 +0100

    create items list page and add item page

---
+++ lib/features/items/presentations/pages/add_item_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mon_marche_domestique/features/items/presentations/bloc/item_bloc.dart';
+import 'package:mon_marche_domestique/features/items/presentations/bloc/item_event.dart';
+
+class AddItemPage extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController markController = TextEditingController();
+  final TextEditingController quantityController = TextEditingController();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Add Item')),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(labelText: 'Name'),
+            ),
+            TextField(
+              controller: markController,
+              decoration: InputDecoration(labelText: 'mark'),
+            ),
+            TextField(
+              controller: quantityController,
+              decoration: InputDecoration(labelText: 'Quantity'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                final name = nameController.text;
+                final mark = markController.text;
+                final quantity = quantityController.text;
+                context.read<ItemBloc>().add(AddItemEvent(name: name, mark: mark, quantity: quantity));
+                Navigator.pop(context);  // Return to item list
+              },
+              child: Text('Add Item'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/items/presentations/pages/items_list_page.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:mon_marche_domestique/features/items/presentations/bloc/item_bloc.dart';
+import 'package:mon_marche_domestique/features/items/presentations/bloc/item_state.dart';
+
+class ItemListPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Item List')),
+      body: BlocBuilder<ItemBloc, ItemState>(
+        builder: (context, state) {
+          if (state is ItemLoadingState) {
+            return Center(child: CircularProgressIndicator());
+          } else if (state is ItemLoadedState) {
+            return ListView.builder(
+              itemCount: state.items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(state.items[index].name),
+                  subtitle: Text(state.items[index].mark),
+                );
+              },
+            );
+          } else if (state is ItemErrorState) {
+            return Center(child: Text(state.message));
+          }
+          return Container();
+        },
+      ),
+    );
+  }
+}
