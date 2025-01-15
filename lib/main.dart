d8537b982ab6269f5f434f587c9f99b68d6289e9
Ghofran <gh.malket@gmail.com>
2025-01-15 17:07:05 +0100

    Initialize Firebase in out application

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // lib/main.dart
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mon_marche_domestique/features/items/data/repository/item_repositoy_impl.dart';
@@ -10,13 +12,16 @@ import 'package:mon_marche_domestique/features/items/presentations/bloc/item_blo
 import 'package:mon_marche_domestique/features/items/presentations/pages/add_item_page.dart';
 import 'package:mon_marche_domestique/features/items/presentations/pages/items_list_page.dart';
 
-void main() {
+void main() async {
   final itemRepository = ItemRepositoryImpl();
   final getItems = GetItems(itemRepository);
   final addItem = AddItem(itemRepository);
   final increaseQuantity = IncreaseItemsQuantity(itemRepository);
   final decreaseQuantity = DecreaseItemsQuantity(itemRepository);
-
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp(
     getItems: getItems,
     addItem: addItem,
