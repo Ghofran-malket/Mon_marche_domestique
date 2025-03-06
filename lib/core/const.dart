2a0afdade62e27ec38be67e6709f3de5469daaf6
Ghofran Malkat <gh.malket@gmail.com>
2025-03-07 00:57:04 +0100

    Add firebase-options and google-services to gitignore file

--- lib/core/const.dart
+++ lib/core/const.dart
@@ -2,3 +2,4 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 String stripePublishableKey = dotenv.env['STRIPE_PUBLISHABLE_KEY'] ?? "";
 String stripeSecretKey = dotenv.env['STRIPE_SECRET_KEY'] ?? "";
+final String apiKey = dotenv.env['API_KEY'] ?? "default-api-key";
