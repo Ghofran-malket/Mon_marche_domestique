aa53904cabd397a2dcc8a554275899cc29e90c78
Ghofran Malkat <gh.malket@gmail.com>
2025-03-07 01:26:57 +0100

    Revoke api keys

--- lib/core/const.dart
+++ lib/core/const.dart
@@ -2,4 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 String stripePublishableKey = dotenv.env['STRIPE_PUBLISHABLE_KEY'] ?? "";
 String stripeSecretKey = dotenv.env['STRIPE_SECRET_KEY'] ?? "";
-final String apiKey = dotenv.env['API_KEY'] ?? "default-api-key";
+final String androidApiKey = dotenv.env['ANDROID_API_KEY'] ?? "default-api-key";
+final String webApiKey = dotenv.env['WEP_API_KEY'] ?? "";
+final String iosApiKey = dotenv.env['IOS_API_KEY'] ?? "";
